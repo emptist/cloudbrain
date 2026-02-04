a987a83feb24e218eb68d96534c879b15e4158cf
emptist <jigme1968@gmail.com>
2026-02-04 18:51:44 +0800

    refactor: Remove FTS triggers from ai_brain_state_schema.sql

--- server/ai_brain_state_schema.sql
+++ server/ai_brain_state_schema.sql
@@ -116,29 +116,3 @@ CREATE INDEX IF NOT EXISTS idx_learning_ai ON ai_learning_progress(ai_id);
 CREATE INDEX IF NOT EXISTS idx_learning_topic ON ai_learning_progress(topic);
 CREATE INDEX IF NOT EXISTS idx_collab_history_ai ON ai_collaboration_history(ai_id);
 CREATE INDEX IF NOT EXISTS idx_collab_history_session ON ai_collaboration_history(session_id);
-
--- Full-text search for thoughts
-CREATE VIRTUAL TABLE IF NOT EXISTS ai_thought_history_fts USING fts5(thought_content, detail=full);
-
--- Trigger to keep FTS index updated for thoughts
-CREATE TRIGGER IF NOT EXISTS ai_thought_history_fts_insert 
-AFTER INSERT ON ai_thought_history 
-BEGIN
-    INSERT INTO ai_thought_history_fts(rowid, thought_content) 
-    VALUES(new.id, new.thought_content);
-END;
-
-CREATE TRIGGER IF NOT EXISTS ai_thought_history_fts_update 
-AFTER UPDATE OF thought_content ON ai_thought_history 
-BEGIN
-    UPDATE ai_thought_history_fts 
-    SET thought_content = new.thought_content 
-    WHERE rowid = old.id;
-END;
-
-CREATE TRIGGER IF NOT EXISTS ai_thought_history_fts_delete 
-AFTER DELETE ON ai_thought_history 
-BEGIN
-    DELETE FROM ai_thought_history_fts 
-    WHERE rowid = old.id;
-END;
